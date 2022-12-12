195c93f11a30c1e54a7ed72a50c74a14814bc6ec
Shoug <Shoug.turki1@gmail.com>
2022-12-12 10:03:18 +0300

    Initial Commit

---
+++ PNU_Train_V0.2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PNU_Train_V0.2
+//
+//  Created by Shouq Turki Bin Tuwaym on 18/05/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PNU_Train_V0.2/PNU_Train_V0_2App.swift
@@ -0,0 +1,17 @@
+//
+//  PNU_Train_V0_2App.swift
+//  PNU_Train_V0.2
+//
+//  Created by Shouq Turki Bin Tuwaym on 18/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct PNU_Train_V0_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
